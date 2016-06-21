343269a8221f7d7fae38affe9690e62ad28e0ad5
Yuddistira Kiki <yuddistira.kiki@gmail.com>
2016-06-21 15:08:42 +0700

    Revert "set LevelClass to ListSettings.java"
    This reverts commit fc8c9ba9b1aab266180a5d2004ccced276806e74.

--- app/src/main/java/com/sgo/orimakardaya/fragments/FragPayFriendsConfirm.java
+++ app/src/main/java/com/sgo/orimakardaya/fragments/FragPayFriendsConfirm.java
@@ -257,10 +257,12 @@ public class FragPayFriendsConfirm extends Fragment implements ReportBillerDialo
 
             if(!list_non_member.isEmpty()) {
 
-                String messageDialog = getString(R.string.dialog_p2p_nonmember_msg,
-                        number_recipient_nonmember,getString(R.string.appname),expired_period_date) ;
+                String messageDialog = getString(R.string.dialog_p2p_nonmember_msg1) + " " + number_recipient_nonmember+ " "+
+                        getString(R.string.dialog_p2p_nonmember_msg2)+" "+
+                        expired_period_date+" "+
+                        getString(R.string.dialog_p2p_nonmember_msg3);
 
-                mDialogNonMember = AlertDialogFrag.newInstance(getString(R.string.dialog_p2p_nonmember_title,getString(R.string.appname)), messageDialog,
+                mDialogNonMember = AlertDialogFrag.newInstance(getString(R.string.dialog_p2p_nonmember_title), messageDialog,
                         getString(R.string.yes),getString(R.string.cancel),false);
                 mDialogNonMember.setOkListener(new DialogInterface.OnClickListener() {
                     @Override
