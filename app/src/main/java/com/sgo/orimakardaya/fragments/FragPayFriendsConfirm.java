fc8c9ba9b1aab266180a5d2004ccced276806e74
Yuddistira Kiki <yuddistira.kiki@gmail.com>
2016-06-21 15:08:14 +0700

    set LevelClass to ListSettings.java

--- app/src/main/java/com/sgo/orimakardaya/fragments/FragPayFriendsConfirm.java
+++ app/src/main/java/com/sgo/orimakardaya/fragments/FragPayFriendsConfirm.java
@@ -257,12 +257,10 @@ public class FragPayFriendsConfirm extends Fragment implements ReportBillerDialo
 
             if(!list_non_member.isEmpty()) {
 
-                String messageDialog = getString(R.string.dialog_p2p_nonmember_msg1) + " " + number_recipient_nonmember+ " "+
-                        getString(R.string.dialog_p2p_nonmember_msg2)+" "+
-                        expired_period_date+" "+
-                        getString(R.string.dialog_p2p_nonmember_msg3);
+                String messageDialog = getString(R.string.dialog_p2p_nonmember_msg,
+                        number_recipient_nonmember,getString(R.string.appname),expired_period_date) ;
 
-                mDialogNonMember = AlertDialogFrag.newInstance(getString(R.string.dialog_p2p_nonmember_title), messageDialog,
+                mDialogNonMember = AlertDialogFrag.newInstance(getString(R.string.dialog_p2p_nonmember_title,getString(R.string.appname)), messageDialog,
                         getString(R.string.yes),getString(R.string.cancel),false);
                 mDialogNonMember.setOkListener(new DialogInterface.OnClickListener() {
                     @Override
