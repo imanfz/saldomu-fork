a42510fcc8439546b7ece4e6c90c4aef97a4b0e9
Yuddistira Kiki <yuddistira.kiki@gmail.com>
2016-10-18 15:52:50 +0700

    #9: Fix up Navigation drawer select item With ENUM

--- app/src/main/java/com/sgo/orimakardaya/Beans/navdrawmainmenuModel.java
+++ app/src/main/java/com/sgo/orimakardaya/Beans/navdrawmainmenuModel.java
@@ -4,50 +4,61 @@ package com.sgo.orimakardaya.Beans;/*
 
 public class navdrawmainmenuModel {
 
-  private String title;
+    private String title;
+    private int navItemId;
+    private int indexImage;
+    private int indexImageSelected;
 
-  private int indexImage;
-  private int indexImageSelected;
+    private boolean isGroupHeader = false;
 
-  private boolean isGroupHeader = false;
-
-  public navdrawmainmenuModel(int _indexImage,int _indexImageSelected,String title,Boolean _isGroupHeader) {
-    this.setTitle(title);
-    if(_isGroupHeader)setGroupHeader(true);
-    else {
-        setGroupHeader(false);
-        this.setIndexImage(_indexImage);
-        if(_indexImageSelected != 0)
-            this.setIndexImageSelected(_indexImageSelected);
-        else
-            this.setIndexImageSelected(_indexImage);
+    public navdrawmainmenuModel(String title) {
+        setUpData(0,0,title,true,-1);
     }
-  }
+
+    public navdrawmainmenuModel(int _indexImage, int _indexImageSelected,String title, int itemId) {
+        setUpData(_indexImage,_indexImageSelected,title,false,itemId);
+    }
+
+    private void setUpData(int _indexImage, int _indexImageSelected, String title, Boolean _isGroupHeader, int itemId){
+        this.setTitle(title);
+        this.setNavItemId(itemId);
+
+        if(_isGroupHeader)
+            setGroupHeader(true);
+        else {
+            setGroupHeader(false);
+            this.setIndexImage(_indexImage);
+            if(_indexImageSelected != 0)
+                this.setIndexImageSelected(_indexImageSelected);
+            else
+                this.setIndexImageSelected(_indexImage);
+        }
+    }
 
 
-  public String getTitle() {
+    public String getTitle() {
     return title;
-  }
+    }
 
-  public void setTitle(String title) {
+    public void setTitle(String title) {
     this.title = title;
-  }
+    }
 
-  public boolean isGroupHeader() {
+    public boolean isGroupHeader() {
     return isGroupHeader;
-  }
+    }
 
-  public void setGroupHeader(boolean isGroupHeader) {
+    public void setGroupHeader(boolean isGroupHeader) {
     this.isGroupHeader = isGroupHeader;
-  }
+    }
 
-  public int getIndexImage() {
+    public int getIndexImage() {
     return indexImage;
-  }
+    }
 
-  public void setIndexImage(int indexImage) {
+    public void setIndexImage(int indexImage) {
     this.indexImage = indexImage;
-  }
+    }
 
     public int getIndexImageSelected() {
         return indexImageSelected;
@@ -55,5 +66,13 @@ public class navdrawmainmenuModel {
 
     public void setIndexImageSelected(int indexImageSelected) {
         this.indexImageSelected = indexImageSelected;
+    }
+
+    public int getNavItemId() {
+        return navItemId;
+    }
+
+    public void setNavItemId(int navItemId) {
+        this.navItemId = navItemId;
     }
 }
