03f51036b373613c8a8223fda9b4aa3f362b509d
LENOVO-PC\Yessica <yessica@sgo.co.id>
2017-04-26 16:58:14 +0700

    Revert "beli pulsa->metode pembayaran scash->akardaya"
    This reverts commit 91f3c20caed80a319b9cfab16b5aee35b739ca5a.

--- app/src/main/java/com/sgo/orimakardaya/fragments/PulsaAgentDescription.java
+++ app/src/main/java/com/sgo/orimakardaya/fragments/PulsaAgentDescription.java
@@ -385,7 +385,7 @@ public class PulsaAgentDescription extends Fragment {
                                     );
 
                                     if(mBob.getProduct_code().equals(DefineValue.SCASH)){
-                                        String tempProductName ="AKARDAYA";
+                                        String tempProductName = mBob.getProduct_name();
                                         mDataPayment.add(mBob);
                                         paymentData.add(tempProductName);
                                     }
