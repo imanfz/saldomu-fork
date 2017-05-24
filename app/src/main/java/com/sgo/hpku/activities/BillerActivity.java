a0c847ab74a15c69f8e0c4c611fdd1b8ba373ada
LENOVO-PC\Yessica <yessica@sgo.co.id>
2017-05-24 15:45:05 +0700

    fix "Can not perform this action after onSaveInstanceState" di biller

--- app/src/main/java/com/sgo/hpku/activities/BillerActivity.java
+++ app/src/main/java/com/sgo/hpku/activities/BillerActivity.java
@@ -190,7 +190,7 @@ public class BillerActivity extends BaseActivity {
         fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.biller_content, mLBM,tag);
-        fragmentTransaction.commit();
+        fragmentTransaction.commitAllowingStateLoss();
         setResult(MainPage.RESULT_NORMAL);
 
     }
@@ -217,14 +217,14 @@ public class BillerActivity extends BaseActivity {
                     .beginTransaction()
                     .replace(R.id.biller_content, mFragment, tag)
                     .addToBackStack(fragName)
-                    .commit();
+                    .commitAllowingStateLoss();
         }
         else {
             Timber.d("bukan backstack:"+"masuk");
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.biller_content, mFragment, tag)
-                    .commit();
+                    .commitAllowingStateLoss();
 
         }
         if(next_frag_title!=null)setActionBarTitle(next_frag_title);
