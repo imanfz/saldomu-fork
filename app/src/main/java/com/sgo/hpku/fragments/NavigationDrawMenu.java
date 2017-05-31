d71fa8efa499966d2ab7234949fbb3715f12c8b1
LENOVO-PC\Yessica <yessica@sgo.co.id>
2017-05-31 10:01:03 +0700

    ganti beberapa icon tambah icon di navdrawer

--- app/src/main/java/com/sgo/hpku/fragments/NavigationDrawMenu.java
+++ app/src/main/java/com/sgo/hpku/fragments/NavigationDrawMenu.java
@@ -247,13 +247,13 @@ public class NavigationDrawMenu extends ListFragment{
     private ArrayList<navdrawmainmenuModel> generateData(){
         ArrayList<navdrawmainmenuModel> models = new ArrayList<>();
         models.add(new navdrawmainmenuModel(getString(R.string.menu_group_title_main_menu)));
-        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_icon_color, R.drawable.ic_cashout_icon_color, getString(R.string.menu_item_title_bbs), MBBS));
+        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_icon_color, R.drawable.ic_topup_icon_color, getString(R.string.menu_item_title_bbs), MBBS));
 
-        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_icon_color,0,getString(R.string.menu_item_title_topup),MTOPUP));              //1
-        models.add(new navdrawmainmenuModel(R.drawable.ic_payfriends_icon_color,0,getString(R.string.menu_item_title_pay_friends),MPAYFRIENDS));    //2
-        models.add(new navdrawmainmenuModel(R.drawable.ic_ask_icon_color,0,getString(R.string.menu_item_title_ask_for_money),MASK4MONEY));            //3
-        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_pulsa,0,getString(R.string.menu_item_title_pulsa_agent), MDAP));
-        models.add(new navdrawmainmenuModel(R.drawable.ic_buy_icon_color,0,getString(R.string.menu_item_title_buy),MBUY));             //4
+        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_icon_color, R.drawable.ic_topup_icon_color, getString(R.string.menu_item_title_topup),MTOPUP));              //1
+        models.add(new navdrawmainmenuModel(R.drawable.ic_payfriends_icon_color,R.drawable.ic_payfriends_icon_color,getString(R.string.menu_item_title_pay_friends),MPAYFRIENDS));    //2
+        models.add(new navdrawmainmenuModel(R.drawable.ic_ask_icon_color,R.drawable.ic_ask_icon_color,getString(R.string.menu_item_title_ask_for_money),MASK4MONEY));            //3
+        models.add(new navdrawmainmenuModel(R.drawable.ic_topup_pulsa,R.drawable.ic_topup_pulsa,getString(R.string.menu_item_title_pulsa_agent), MDAP));
+        models.add(new navdrawmainmenuModel(R.drawable.ic_buy_icon_color,R.drawable.ic_buy_icon_color,getString(R.string.menu_item_title_buy),MBUY));             //4
 //        models.add(new navdrawmainmenuModel(R.drawable.ic_cashout_icon_color,0,getString(R.string.menu_item_title_cash_out),false));       //5
 
 
@@ -262,11 +262,11 @@ public class NavigationDrawMenu extends ListFragment{
 //        models.add(new navdrawmainmenuModel(R.drawable.ic_groups_icon_color,0,getString(R.string.menu_item_title_my_groups),false));          //9
 
         models.add(new navdrawmainmenuModel(getString(R.string.menu_group_title_supports)));                                        //10
-        models.add(new navdrawmainmenuModel(R.drawable.ic_report,0,getString(R.string.menu_item_title_report),MREPORT));              //6
-        models.add(new navdrawmainmenuModel(R.drawable.ic_setting,0,getString(R.string.menu_item_title_setting),MSETTINGS));                    //11
-        models.add(new navdrawmainmenuModel(R.drawable.ic_help,0,getString(R.string.menu_item_title_help),MHELP));                          //12
+        models.add(new navdrawmainmenuModel(R.drawable.ic_report,R.drawable.ic_report,getString(R.string.menu_item_title_report),MREPORT));              //6
+        models.add(new navdrawmainmenuModel(R.drawable.ic_setting,R.drawable.ic_setting,getString(R.string.menu_item_title_setting),MSETTINGS));                    //11
+        models.add(new navdrawmainmenuModel(R.drawable.ic_help,R.drawable.ic_help,getString(R.string.menu_item_title_help),MHELP));                          //12
         models.add(new navdrawmainmenuModel(getString(R.string.menu_group_title_logout)));                                        //13
-        models.add(new navdrawmainmenuModel(R.drawable.ic_logout_icon,0,getString(R.string.menu_item_title_logout),MLOGOUT));                 //14
+        models.add(new navdrawmainmenuModel(R.drawable.ic_logout_icon,R.drawable.ic_logout_icon,getString(R.string.menu_item_title_logout),MLOGOUT));                 //14
 
         return models;
     }
