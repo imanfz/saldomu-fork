4168c8c9938456bffa6609dfed8ed2fa49d9105b
Yuddistira Kiki <yuddistira.kiki@gmail.com>
2017-08-08 10:00:49 +0700

    setup file untuk background data BBS

---
+++ app/src/main/java/com/sgo/saldomu/receivers/LocalResultReceiver.java
@@ -0,0 +1,10 @@
+package com.sgo.saldomu.receivers;
+
+import android.os.ResultReceiver;
+
+/**
+ * Created by yuddistirakiki on 8/7/17.
+ */
+
+public class LocalResultReceiver extends ResultReceiver {
+}
