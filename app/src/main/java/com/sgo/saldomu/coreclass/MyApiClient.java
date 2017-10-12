dd4ad8c3c51c790fb0d0dc5d8f5d3462c6d4f603
LENOVO-PC\Yessica <yessica@sgo.co.id>
2017-10-12 15:38:55 +0700

    ubah FAQ dan terms n condition ke url saldomu

--- app/src/main/java/com/sgo/saldomu/coreclass/MyApiClient.java
+++ app/src/main/java/com/sgo/saldomu/coreclass/MyApiClient.java
@@ -363,12 +363,12 @@ public class MyApiClient {
 
     public static String URL_HELP_DEV = "https://mobile-dev.espay.id/static/pages/help/";
     public static String URL_FAQ;
-    public static String URL_FAQ_PROD = "https://mobile.goworld.asia/static/pages/help/pin_faq_akardaya.html";
-    public static String URL_FAQ_DEV = URL_HELP_DEV +"pin_faq_akardaya.html";
+    public static String URL_FAQ_PROD = "https://mobile.espay.id/static/pages/help/pin_faq_saldomu.html";
+    public static String URL_FAQ_DEV = URL_HELP_DEV +"pin_faq_saldomu.html";
 
     public static String URL_TERMS;
-    public static String URL_TERMS_PROD = "https://mobile.goworld.asia/static/pages/help/pin_terms_conditions_id_akardaya.html";
-    public static String URL_TERMS_DEV = URL_HELP_DEV +"pin_terms_conditions_id_akardaya.html";
+    public static String URL_TERMS_PROD = "https://mobile.espay.id/static/pages/help/pin_terms_conditions_id_saldomu.html";
+    public static String URL_TERMS_DEV = URL_HELP_DEV +"pin_terms_conditions_id_saldomu.html";
 
 
 
