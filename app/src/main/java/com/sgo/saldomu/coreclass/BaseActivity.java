d79ea36f76f9e9b0243bc8537340f9935b51efd3
Kelvin p <kevinp@sgo.co.id>
2018-04-02 11:32:48 +0700

    ubah struktur code

--- app/src/main/java/com/sgo/saldomu/coreclass/BaseActivity.java
+++ app/src/main/java/com/sgo/saldomu/coreclass/BaseActivity.java
@@ -39,19 +39,28 @@ public abstract class BaseActivity extends AppCompatActivity implements EasyPerm
             Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private IntentFilter fcmFilter = new IntentFilter();
     FcmReceiver fcmReceiver = new FcmReceiver();
-    private SecurePreferences sp;
 
     protected PermissionResult permissionResultInterface = this;
+
+    protected SecurePreferences sp;
+    protected String memberIDLogin, commIDLogin, userPhoneID, accessKey;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
 
-        detoolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        deprogressbar = (ProgressBar) findViewById(R.id.main_toolbar_progress_spinner);
+        sp = CustomSecurePref.getInstance().getmSecurePrefs();
 
-        title_detoolbar = (TextView) findViewById(R.id.main_toolbar_title);
+        memberIDLogin = sp.getString(DefineValue.MEMBER_ID,"");
+        commIDLogin = sp.getString(DefineValue.COMMUNITY_ID,"");
+        userPhoneID = sp.getString(DefineValue.USERID_PHONE,"");
+        accessKey = sp.getString(DefineValue.ACCESS_KEY, "");
+
+        detoolbar = findViewById(R.id.main_toolbar);
+        deprogressbar = findViewById(R.id.main_toolbar_progress_spinner);
+
+        title_detoolbar = findViewById(R.id.main_toolbar_title);
         if (detoolbar != null) {
             setSupportActionBar(detoolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
