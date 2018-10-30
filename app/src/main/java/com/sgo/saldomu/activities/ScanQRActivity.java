95cc411fb9b7574d2abe786f28e4e88461015f0f
SGO-Yessica <yessica@sgo.co.id>
2018-10-30 14:35:29 +0700

    - fixing Fc jika scan QR asal - perbesar image Qrcode agar bisa discan oleh tab

--- app/src/main/java/com/sgo/saldomu/activities/ScanQRActivity.java
+++ app/src/main/java/com/sgo/saldomu/activities/ScanQRActivity.java
@@ -233,8 +233,10 @@ public class ScanQRActivity extends AppCompatActivity implements ZXingScannerVie
 //        editor.putString(QRVALUE, rawResult.getText());
 //        editor.apply();
 
-
-        if(!qrType.isEmpty()){
+        if(qrType==null) {
+            Toast.makeText(ScanQRActivity.this, "QrCode Tidak Sesuai !",Toast.LENGTH_SHORT).show();
+        }
+        else if(!qrType.isEmpty()){
             QrModel qrModel = new QrModel(benef, benefName, qrType);
             Timber.d("Isi qrOBJ name:"+qrModel.getSourceName()+" id:"+qrModel.getSourceAcct()+" type:"+qrModel.getQrType());            Intent intent = new Intent();
             intent.putExtra(DefineValue.QR_OBJ, qrModel);
