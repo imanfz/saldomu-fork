0132814bc420644f02c9b9e0dba98bbccfb3660f
SGO-Yessica <yessica@sgo.co.id>
2018-11-29 17:31:58 +0700

    - xml untuk dialog input amount - xml untuk preview pembayaran invoice

--- app/src/main/java/com/sgo/saldomu/dialogs/InputAmountTagihBillerDialog.java
+++ app/src/main/java/com/sgo/saldomu/dialogs/InputAmountTagihBillerDialog.java
@@ -6,20 +6,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.sgo.saldomu.R;
 import com.sgo.saldomu.models.InvoiceDGI;
 
 public class InputAmountTagihBillerDialog extends DialogFragment {
 
-    View v;
+    View view;
     OnTap listener;
     InvoiceDGI obj;
     int pos;
+    EditText inpAmount;
+    Button btnDone,btnCancel;
 
     public interface OnTap{
         void onTap(int pos, String value);
@@ -47,8 +53,35 @@ public class InputAmountTagihBillerDialog extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.item_invoice, container, false);
-        return v;
+        view = inflater.inflate(R.layout.dialog_input_invoicedgi, container, false);
+
+        getDialog().setTitle(getString(R.string.invoice_dgi_payment));
+        inpAmount             = (EditText) view.findViewById(R.id.inpAmount);
+        btnDone               = (Button) view.findViewById(R.id.btnDone);
+        btnCancel             = (Button) view.findViewById(R.id.btnCancel);
+
+
+        TextView lbl_doc_no = (TextView) view.findViewById(R.id.lbl_doc_no);
+//        lbl_doc_no.setText(doc_no);
+
+        TextView lbl_doc_desc = (TextView) view.findViewById(R.id.lbl_doc_desc);
+//        lbl_doc_desc.setText(doc_desc);
+
+        TextView lbl_amount = (TextView) view.findViewById(R.id.lbl_amount);
+//        lbl_amount.setText(amount);
+
+        TextView lbl_remain_amount = (TextView) view.findViewById(R.id.lbl_remain_amount);
+//        lbl_remain_amount.setText(remain_amount);
+
+        TextView lbl_partial = (TextView) view.findViewById(R.id.lbl_partial);
+//        lbl_partial.setText(partial);
+
+//        if(partial.equalsIgnoreCase("Ya") || partial.equalsIgnoreCase("Bisa Lebih")){
+//            showInvoiceAmount(view);
+//        }else{
+//            hideInvoiceAmount(view);
+//        }
+        return view;
     }
 
     @Override
