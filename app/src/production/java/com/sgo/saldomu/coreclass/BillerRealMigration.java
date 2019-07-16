ebe9e1c52daffb54e5fd9e413ea5e4ec6c4f3ec2
Yessica Octavia Hermawan <yessica@sgo.co.id>
2019-07-16 15:56:54 +0700

    realm

--- app/src/production/java/com/sgo/saldomu/coreclass/BillerRealMigration.java
+++ app/src/production/java/com/sgo/saldomu/coreclass/BillerRealMigration.java
@@ -25,7 +25,7 @@ public class BillerRealMigration implements RealmMigration {
 
             RealmObjectSchema billerDataModel = schema.get("Biller_Data_Model");
             billerDataModel.addField("manual_advice",String.class, FieldAttribute.REQUIRED);
-            billerDataModel.addField("biller_info",String.class);
+//            billerDataModel.addField("biller_info",String.class);
         }
     }
 }
