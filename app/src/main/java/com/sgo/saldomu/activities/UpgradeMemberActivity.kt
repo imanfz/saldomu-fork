2fed97edf1fe0ab08e8b1f8a76aa5f1a4fdea3bc
Yessica Octavia Hermawan <yessica@sgo.co.id>
2019-08-13 10:48:19 +0700

    upgrade member via agent (undone)

--- app/src/main/java/com/sgo/saldomu/activities/UpgradeMemberActivity.kt
+++ app/src/main/java/com/sgo/saldomu/activities/UpgradeMemberActivity.kt
@@ -3,11 +3,16 @@ package com.sgo.saldomu.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import com.securepreferences.SecurePreferences
 import com.sgo.saldomu.R
+import com.sgo.saldomu.coreclass.CustomSecurePref
+import com.sgo.saldomu.coreclass.DefineValue
 import com.sgo.saldomu.widgets.BaseActivity
 import kotlinx.android.synthetic.main.activity_upgrade_member.*
 
 class UpgradeMemberActivity : BaseActivity() {
+
+
     override fun getLayoutResource(): Int {
         return R.layout.activity_upgrade_member
     }
@@ -19,6 +24,16 @@ class UpgradeMemberActivity : BaseActivity() {
 
     private fun initialize() {
         actionBarTitle = getString(R.string.menu_item_title_upgrade_via_agent)
+
+        sp = CustomSecurePref.getInstance().getmSecurePrefs()
+
+
+        if (sp.getString(DefineValue.COMM_UPGRADE_MEMBER, "") != null)
+            if (sp.getString(DefineValue.COMM_UPGRADE_MEMBER, "") == "O") {
+                upgrade_via_agent_button.isEnabled == false
+            } else if (sp.getString(DefineValue.COMM_UPGRADE_MEMBER, "") == "A") {
+                upgrade_online_button.isEnabled == false
+            }
 
         upgrade_online_button.setOnClickListener {
             startActivity(Intent(this, MyProfileNewActivity::class.java))
--- app/src/main/java/com/sgo/saldomu/activities/UpgradeMemberViaAgentActivity.kt
+++ app/src/main/java/com/sgo/saldomu/activities/UpgradeMemberViaAgentActivity.kt
@@ -6,12 +6,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import com.sgo.saldomu.R
+import com.sgo.saldomu.coreclass.DefineValue
 import com.sgo.saldomu.widgets.BaseActivity
 import kotlinx.android.synthetic.main.activity_upgrade_member_via_agent.*
 import java.util.*
 
 
 class UpgradeMemberViaAgentActivity: BaseActivity() {
+
     override fun getLayoutResource(): Int {
         return R.layout.activity_upgrade_member_via_agent
     }
@@ -23,6 +25,7 @@ class UpgradeMemberViaAgentActivity: BaseActivity() {
 
     private fun initialize () {
         actionBarTitle = getString(R.string.menu_item_title_upgrade_member)
+
 
         submit_button.setOnClickListener {
             startActivity(Intent(this, ProsesUpgradeMemberActivity::class.java))
