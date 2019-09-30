8e8ce36776470b13e51b8e61c597f9efb99b6f40
Yessica Octavia Hermawan <yessica@sgo.co.id>
2019-09-30 14:36:53 +0700

    delete app_data

--- app/src/main/java/com/sgo/saldomu/models/retrofit/jsonModel.java
+++ app/src/main/java/com/sgo/saldomu/models/retrofit/jsonModel.java
@@ -12,9 +12,9 @@ public class jsonModel {
     private String error_message;
     @SerializedName("on_error")
     private boolean on_error;
-    @SerializedName("app_data")
-    @Expose
-    private List<AppDataModel> app_data = null;
+//    @SerializedName("app_data")
+//    @Expose
+//    private List<AppDataModel> app_data = null;
 
     public String getError_code() {
         if (error_code == null)
@@ -32,7 +32,7 @@ public class jsonModel {
         return on_error;
     }
 
-    public List<AppDataModel> getApp_data() {
-        return app_data;
-    }
+//    public List<AppDataModel> getApp_data() {
+//        return app_data;
+//    }
 }
