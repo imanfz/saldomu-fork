a71391f973375233b66c932f8d51786c477f710a
Albert Lim <albertlim@sgo.co.id>
2021-10-15 15:41:25 +0700

    fix photo camera

--- app/src/main/java/com/sgo/saldomu/utils/PickAndCameraUtil.java
+++ app/src/main/java/com/sgo/saldomu/utils/PickAndCameraUtil.java
@@ -19,6 +19,8 @@ import com.sgo.saldomu.coreclass.GeneralizeImage;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import timber.log.Timber;
 
@@ -33,20 +35,20 @@ public class PickAndCameraUtil {
     private String mCurrentPhotoPath;
     private GeneralizeImage generalizeImage;
 
-    public PickAndCameraUtil(Activity activity){
+    public PickAndCameraUtil(Activity activity) {
         this.mActivity = activity;
     }
 
-    public PickAndCameraUtil(Activity activity,Fragment fragment){
+    public PickAndCameraUtil(Activity activity, Fragment fragment) {
         this.mActivity = activity;
         this.mFragment = fragment;
     }
 
-    public Uri getCaptureImageUri(){
+    public Uri getCaptureImageUri() {
         return mCapturedImageURI;
     }
 
-    public String getCurrentPhotoPath(){
+    public String getCurrentPhotoPath() {
         return mCurrentPhotoPath;
     }
 
@@ -68,23 +70,22 @@ public class PickAndCameraUtil {
 
     }
 
-    public File compressImage(String stringUri){
-        if(generalizeImage == null)
+    public File compressImage(String stringUri) {
+        if (generalizeImage == null)
             generalizeImage = new GeneralizeImage(mActivity);
         return generalizeImage.compressImage(stringUri);
     }
 
-    private void startActivityForResult(Intent i, int type ){
-        if(mFragment != null){
-            mFragment.startActivityForResult(i,type);
-        }
-        else {
-            if(mActivity != null)
-                mActivity.startActivityForResult(i,type);
+    private void startActivityForResult(Intent i, int type) {
+        if (mFragment != null) {
+            mFragment.startActivityForResult(i, type);
+        } else {
+            if (mActivity != null)
+                mActivity.startActivityForResult(i, type);
         }
     }
 
-    private PackageManager getPackageManager(){
+    private PackageManager getPackageManager() {
         return mActivity.getPackageManager();
     }
 
@@ -97,67 +98,57 @@ public class PickAndCameraUtil {
     }
 
 
-    public void runCamera(int reqCode){
+    public void runCamera(int reqCode) {
         Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
-                String timeStamp = DateTimeFormat.getCurrentDateTime();
-                String imageFileName = "JPEG_" + timeStamp + "_" + BuildConfig.APP_ID;
-//                String imageFileName = "IMG_" + timeStamp;
 
-                ContentValues values = new ContentValues();
-                values.put(MediaStore.Images.Media.TITLE, imageFileName);
-
-                mCapturedImageURI = mActivity.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-            }
-            else {
-
-                File photoFile = null;
-                try {
+            File photoFile = null;
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                    photoFile = createImageFile2();
+                else
                     photoFile = createImageFile();
-                } catch (IOException ex) {
-                    Timber.e(ex);
-                }
+            } catch (IOException ex) {
+                Timber.e(ex);
+            }
 
-                if (photoFile != null) {
-                    mCapturedImageURI = FileProvider.getUriForFile(mActivity,
-                            BuildConfig.APPLICATION_ID + ".provider",
-                            photoFile);
-                }
+            if (photoFile != null) {
+                mCapturedImageURI = FileProvider.getUriForFile(mActivity,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        photoFile);
             }
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
             startActivityForResult(takePictureIntent, reqCode);
         }
     }
 
-//    private File createImageFile() throws IOException {
-//        // Create an image file name
-//        String timeStamp = DateTimeFormat.getCurrentDateTime();
-//        String imageFileName = "JPEG_" + timeStamp + "_" + BuildConfig.APP_ID;
-////        String imageFileName = "IMG_" + timeStamp;
-//        File storageDir = mActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-//        File image = File.createTempFile(
-//                imageFileName,  /* prefix */
-//                ".jpg",         /* suffix */
-//                storageDir      /* directory */
-//        );
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath =image.toString();
-//        return image;
-//    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = DateTimeFormat.getCurrentDateTime();
+        String imageFileName = "JPEG_" + timeStamp + "_" + BuildConfig.APP_ID;
+//        String imageFileName = "IMG_" + timeStamp;
+        File storageDir = mActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.toString();
+        return image;
+    }
 
     public static String prepareFileName() {
         String timeStamp = DateTimeFormat.getCurrentDateTime();
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        return imageFileName;
+        return "JPEG_" + timeStamp + "_";
     }
 
-    private static File createImageFile() throws IOException {
+    private File createImageFile2() throws IOException {
         String imageFileName = prepareFileName();
 //        epassBookFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), currentDate + "_" +fileName);
-        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), BuildConfig.APPLICATION_ID+ "Image.JPEG");
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), BuildConfig.APPLICATION_ID + "Image.JPEG");
         storageDir.mkdirs();
 
         if (!storageDir.exists()) {
@@ -172,6 +163,7 @@ public class PickAndCameraUtil {
         );
     }
 
-
-
+    public File prepareUploadFileTemp() throws IOException {
+        return createImageFile2();
+    }
 }
